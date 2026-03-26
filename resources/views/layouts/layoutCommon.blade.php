c5c591470722a34301102528ef3ed37c694e9631
Developer <developer@example.com>
2026-03-26 12:53:33 +0530

    DEBUG: 500 error investigation - restored working layout

--- resources/views/layouts/layoutCommon.blade.php
+++ resources/views/layouts/layoutCommon.blade.php
@@ -1,26 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <x-head />
-
 <body class="custom-cursor">
-
     <div class="custom-cursor__cursor"></div>
     <div class="custom-cursor__cursor-two"></div>
-
     <x-loader />
-    <x-side_content />
-    <x-chat_popup />
-
     <div class="page-wrapper">
         <x-header_style_one />
-
         @yield('content')
-
         <x-mobile_nav />
+        <x-side_content />
+        <x-chat_popup />
         <x-search_popup />
         <x-scroll_to_top />
         <x-scripts />
-
+    </div>
 </body>
-
 </html>
