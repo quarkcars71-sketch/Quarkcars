5ae601e7da02619e162503fefabc894123717e03
Developer <developer@example.com>
2026-04-03 11:57:18 +0530

    update the favicon icon

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -7,7 +7,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicons/favicon-32x32.png') }}" />
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicons/favicon-16x16.png') }}" />
     <link rel="manifest" href="{{ asset('assets/images/favicons/site.webmanifest') }}" />
-    <meta name="description" content="Gorent Laravel  Template " />
+    <meta name="description" content="QuarkCars Laravel  Template " />
 
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
