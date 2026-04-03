60697f9e4fc666c507608e78206ed301a19ebeca
Developer <developer@example.com>
2026-04-03 14:40:31 +0530

    update the logo of mobile nav

--- resources/views/components/mobile_nav.blade.php
+++ resources/views/components/mobile_nav.blade.php
@@ -6,7 +6,7 @@
 
         <div class="logo-box">
             <a href="{{ route('index') }}" aria-label="logo image"><img
-                    src="{{ asset('assets/images/resources/logo-2(1).png') }}" width="140" alt="" /></a>
+                    src="{{ asset('assets/images/resources/logo-2(2).png') }}" width="140" alt="" /></a>
         </div>
         <!-- /.logo-box -->
         <div class="mobile-nav__container"></div>
