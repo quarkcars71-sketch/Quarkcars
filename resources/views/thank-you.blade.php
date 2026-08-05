76c60c099bc94fe51a21924743ca389db836bd94
quarkcars71-sketch <quarkcars71@gmail.com>
2026-08-05 13:16:16 +0530

    Integrate Google Tag Manager into thank-you page
    Added Google Tag Manager script and noscript tags to the thank-you page.

--- resources/views/thank-you.blade.php
+++ resources/views/thank-you.blade.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Thank You - QuarkCars</title>
-    <meta charset="UTF-8">
+    
+<!-- Google Tag Manager -->
+<script>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5PDNJG7V');
+</script>
+<!-- End Google Tag Manager -->
+
+<title>Thank You - QuarkCars</title>
+<meta charset="UTF-8">
+    
     <style>
         body{
             font-family: Arial, sans-serif;
@@ -34,6 +46,13 @@
     </style>
 </head>
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PDNJG7V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager -->
 
 <div class="box">
     <h1>🎉 Thank You!</h1>
